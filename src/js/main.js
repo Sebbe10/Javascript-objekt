95d3e1eb575676f737ae6401d0e94b10ba155d8e
Sebastian Bjelk <sebastian.bjelkstam@gmail.com>
2022-10-26 13:36:56 +0200

    Javascript objekt

---
+++ src/js/main.js
@@ -0,0 +1,15 @@
+class Person {
+    firstname;
+    age;
+    interest;
+}
+
+window.onload = function() {
+    let person = {
+        firstname:"Sebbe",
+        age:21,
+        interest:"tennis",
+    }
+
+    let names = [Person, person];
+}
